779e0c3cffb5fb7916fa6e8c62c250e980d2d524
Rocco Caricola <rocco.caricola89@gmail.com>
2025-02-24 15:50:35 +0100

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    extensions: [".js", ".ts", ".jsx", ".tsx", ".scss"], // Ensure .scss is included
+  },
 })
